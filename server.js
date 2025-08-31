5fb025ea12d4f671c98580df62f60929bf289177
Mohamed Aziz Idoudi <mohamedaziz.idoudi@etudiant-fst.utm.tn>
2025-08-31 15:41:04 +0100

    port change

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
 const hostname = process.env.NODE_ENV !== 'production' ?'localhost' : "groupetmholding.com"
-const port = process.env.PORT || 3000 
+const port = process.env.PORT || 3010 
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
